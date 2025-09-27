f44a07142aa250822f3653b4af7f3ad67df783cf
Yingjie Zhu <57340458+AAAndy-Zhu@users.noreply.github.com>
2025-09-27 23:36:52 +0800

    Add files via upload

---
+++ scripts/Gemma3/similarity.py
@@ -0,0 +1,82 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = '3'
+
+from PIL import Image
+import requests
+from tqdm import tqdm
+import json
+import torch
+
+from transformers import AutoProcessor, Gemma3ForConditionalGeneration
+import argparse
+
+
+def main(args):
+
+    model_id = args.model_path
+
+    model = Gemma3ForConditionalGeneration.from_pretrained(
+        model_id, device_map="auto"
+    ).eval()
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    model.position_balance = False
+
+    eval_file_path = args.eval_file
+    img_path = args.img_path
+    answers_file = args.answers_file
+
+    test_data = [json.loads(line) for line in open(eval_file_path)]
+    test_data = test_data[:9000]
+    ans_file = open(answers_file, "w")
+    for data in tqdm(test_data):
+        caption = data['caption']
+        image_file = os.path.join(img_path, data['image'])
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "image": image_file,
+                    },
+                    {"type": "text", "text": caption},
+                ],
+            }
+        ]
+
+        inputs = processor.apply_chat_template(
+            messages, add_generation_prompt=True, tokenize=True,
+            return_dict=True, return_tensors="pt"
+        ).to(model.device, dtype=torch.bfloat16)
+
+        inputs_text = processor(text=[caption], padding=True, return_tensors="pt").to('cuda')
+        text_embeds = model.get_input_embeddings()(inputs_text['input_ids'])[0]
+        # print(text_embeds.shape)
+        image_embeds = model(**inputs, only_return_image_embeds=True)[0]
+        # print(image_embeds.shape)
+        text_embeds_max = torch.max(text_embeds, dim=0).values  # [hidden_size]
+        image_embeds_max = torch.max(image_embeds, dim=0).values  # [hidden_size]
+
+        similarity = torch.nn.functional.cosine_similarity(
+            text_embeds_max.unsqueeze(0),
+            image_embeds_max.unsqueeze(0)
+        )[0].item()
+
+        # print(f"Similarity: {similarity}")
+        data['similarity'] = similarity
+        ans_file.write(json.dumps(data) + "\n")
+        ans_file.flush()
+    ans_file.close()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, default="./checkpoints/Gemma3")
+    parser.add_argument("--eval_file", type=str, default="../../similarity_data/laion_bias_test_exist_10k_840_840.json")
+    parser.add_argument("--img_path", type=str, default="../../similarity_data/images")
+    parser.add_argument("--answers_file", type=str, default="./similarity_results/gemma3_similarity.json")
+    args = parser.parse_args()
+
+    main(args)
