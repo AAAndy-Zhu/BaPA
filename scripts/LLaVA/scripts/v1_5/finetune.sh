9f80837a424d2387da53fae0bc0bf80890fe4ae6
Yingjie Zhu <57340458+AAAndy-Zhu@users.noreply.github.com>
2025-09-28 17:53:27 +0800

    Change output directory for finetuning script

--- scripts/LLaVA/scripts/v1_5/finetune.sh
+++ scripts/LLaVA/scripts/v1_5/finetune.sh
@@ -15,7 +15,7 @@ deepspeed --master_port=5195 llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-7b-position-1-scratch-new \
+    --output_dir ./checkpoints/llava-v1.5-7b-bapa-scratch \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16  \
     --per_device_eval_batch_size 4 \
